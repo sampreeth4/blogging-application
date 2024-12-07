dbaab96a78452bfe302a7d643d501ab4ba6078a5
sampreeth <saisampreeth78@gmail.com>
2024-12-07 18:10:13 +0530

    updated

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const PORT = 8000;
+const path = require('path');
 const mongoose = require('mongoose');
-
+const userRoute = require("./routers/user");
 
 const app = express();
 
 app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+app.use(express.urlencoded({extended: false}));
 
+mongoose
+    .connect("mongodb://localhost:27017/blogify")
+    .then((e) => console.log("MongoDB connected"));
 
-app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
+app.get("/", (req,res) => {
+    res.render("home");
+});
+
+app.use("/user", userRoute);
+
+app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
