1c05a2ab047c9ea2e45d77a5b4f6a0d87d09b539
sampreeth <saisampreeth78@gmail.com>
2024-12-14 17:01:33 +0530

    updated full

--- index.js
+++ index.js
@@ -5,26 +5,29 @@ const path = require("path");
 const PORT = 8000;
 const app = express();
 const userRoute = require("./routes/user");
+const blogRouter = require("./routes/blog");
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
+const Blog = require("./models/blog");
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
 
 mongoose.connect("mongodb://localhost:27017/blogify").then((e) => console.log("MongoDB connected"));
 
-app.get("/", (req,res) => {
+app.get("/", async (req,res) => {
+    const allBlogs = await Blog.find({});
     res.render("home", {
         user: req.user,
+        blogs: allBlogs,
     });
 });
 
 app.use("/user", userRoute);
+app.use("/blog", blogRouter)
 
-// app.get("/signup", (req,res) => {
-//     return res.render("signup");
-// })
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
