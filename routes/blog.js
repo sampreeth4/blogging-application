226eda2edcb74f5e4889544a3a7ed8073b141a27
sampreeth <saisampreeth78@gmail.com>
2024-12-16 17:16:03 +0530

    updated

--- routes/blog.js
+++ routes/blog.js
@@ -27,10 +27,8 @@ router.get("/add-new", (req, res) => {
 
 router.get("/:id", async (req, res) => {
   const blog = await Blog.findById(req.params.id).populate("createdBy");
-  const comments = await Comment.find({ blogId: req.params.id }).populate(
-    "createdBy"
-  );
-//   console.log(comments);
+  const comments = await Comment.find({ blogId: req.params.id }).populate("createdBy");
+  console.log(comments);
 
   return res.render("blog", {
     user: req.user,
